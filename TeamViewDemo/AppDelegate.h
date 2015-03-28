1b8f77d4b875449b51684d4ce178be8f8b73c7bc
avikantz <avikantsainidbz@gmail.com>
2015-03-28 15:07:37 +0530

    Initial Commit

---
+++ TeamViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TeamViewDemo
+//
+//  Created by Avikant Saini on 3/28/15.
+//  Copyright (c) 2015 avikantz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TeamViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TeamViewDemo
+//
+//  Created by Avikant Saini on 3/28/15.
+//  Copyright (c) 2015 avikantz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
